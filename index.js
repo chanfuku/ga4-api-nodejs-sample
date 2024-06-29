9cdc056046c23f3a19f5a546e278060a0850f078
江波戸航平 <koheiebato@gmail.com>
2024-06-29 14:58:22 +0900

    base

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
-  /**
-   * TODO(developer): Uncomment this variable and replace with your
-   *   Google Analytics 4 property ID before running the sample.
-   */
-  propertyId = '444299948';
+// Imports the Google Analytics Data API client library.
+import {BetaAnalyticsDataClient} from '@google-analytics/data';
+import dotenv from 'dotenv';
+dotenv.config();
 
-  // Imports the Google Analytics Data API client library.
-  const {BetaAnalyticsDataClient} = require('@google-analytics/data');
+// Using a default constructor instructs the client to use the credentials
+// specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
+const analyticsDataClient = new BetaAnalyticsDataClient();
 
-  // Using a default constructor instructs the client to use the credentials
-  // specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
-  const analyticsDataClient = new BetaAnalyticsDataClient();
+/**
+ * TODO(developer): Uncomment this variable and replace with your
+ *   Google Analytics 4 property ID before running the sample.
+ */
+const propertyId = '444299948';
 
-  // Runs a simple report.
-  async function runReport() {
-    const [response] = await analyticsDataClient.runReport({
-      property: `properties/${propertyId}`,
-      dateRanges: [
-        {
-          startDate: '2020-03-31',
-          endDate: 'today',
-        },
-      ],
-      dimensions: [
-        {
-          name: 'city',
-        },
-      ],
-      metrics: [
-        {
-          name: 'activeUsers',
-        },
-      ],
-    });
+// Runs a simple report.
+async function runReport() {
+  const [response] = await analyticsDataClient.runReport({
+    property: `properties/${propertyId}`,
+    dateRanges: [
+      {
+        startDate: '2024-06-01',
+        endDate: 'today',
+      },
+    ],
+    dimensions: [
+      {
+        name: 'unifiedPagePathScreen',
+      },
+      // {
+      //   name: 'defaultChannelGroup',
+      // },
+    ],
+    metrics: [
+      {
+          "name": "screenPageViews"
+      },
+      //{
+      //    "name": "sessions"
+      //},
+    ],
+  });
 
-    console.log('Report result:');
-    response.rows.forEach((row) => {
-      console.log(row.dimensionValues[0], row.metricValues[0]);
-    });
-  }
+  console.log('Report result:');
+  response.rows.forEach((row) => {
+    console.log(row.dimensionValues[0], row.metricValues[0]);
+  });
+}
 
-  runReport();
+runReport();
